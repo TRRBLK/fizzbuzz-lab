0241e7588ee9ffc3779fd67e60865ea827fd59be
Jorge T <git.trrblnc@gmail.com>
2025-12-10 12:31:31 -0600

    feat: aplicacion con vite

--- fifth_week/vite_vanilla_project/my-app-learner/src/main.js
+++ fifth_week/vite_vanilla_project/my-app-learner/src/main.js
@@ -16,7 +16,7 @@ document.querySelector('#app').innerHTML = `
       <button id="counter" type="button"></button>
     </div>
     <p class="read-the-docs">
-      Click on the Vite logo to learn more
+      Estoy aprendiendo a usar Vite
     </p>
   </div>
 `
